8ab40b4a8a89b00ed528c105c0c027fd84bdf5e3
Arthur Vieira <Vieiraarthurandrewssantos@gmail.com>
2026-06-25 08:46:13 -0300

    Classes Produto e Fucionarios atualizadas

--- src/Funcionario.php
+++ src/Funcionario.php
@@ -5,12 +5,24 @@ class Funcionario
     private int $id;
     private string $nome;
     private string $cargo;
+    private float $salario;
+    private string $horarioEntrada;
+    private string $horarioSaida;
 
-    public function __construct(int $id, string $nome, string $cargo)
-    {
+    public function __construct(
+        int $id,
+        string $nome,
+        string $cargo,
+        float $salario,
+        string $horarioEntrada,
+        string $horarioSaida
+    ) {
         $this->id = $id;
         $this->nome = $nome;
         $this->cargo = $cargo;
+        $this->salario = $salario;
+        $this->horarioEntrada = $horarioEntrada;
+        $this->horarioSaida = $horarioSaida;
     }
 
     public function getId(): int
@@ -41,5 +53,45 @@ class Funcionario
     public function setCargo(string $cargo): void
     {
         $this->cargo = $cargo;
+    }
+
+    public function getSalario(): float
+    {
+        return $this->salario;
+    }
+
+    public function setSalario(float $salario): void
+    {
+        $this->salario = $salario;
+    }
+
+    public function getHorarioEntrada(): string
+    {
+        return $this->horarioEntrada;
+    }
+
+    public function setHorarioEntrada(string $horarioEntrada): void
+    {
+        $this->horarioEntrada = $horarioEntrada;
+    }
+
+    public function getHorarioSaida(): string
+    {
+        return $this->horarioSaida;
+    }
+
+    public function setHorarioSaida(string $horarioSaida): void
+    {
+        $this->horarioSaida = $horarioSaida;
+    }
+
+    public function aplicarAumento(float $percentual): void
+    {
+        $this->salario += $this->salario * ($percentual / 100);
+    }
+
+    public function calcularBonus(): float
+    {
+        return $this->salario * 0.10;
     }
 }
--- src/Produto.php
+++ src/Produto.php
@@ -4,11 +4,19 @@ class Produto
 {
     private int $id;
     private string $nome;
+    private float $preco;
+    private int $quantidade;
 
-    public function __construct(int $id, string $nome)
-    {
+    public function __construct(
+        int $id,
+        string $nome,
+        float $preco,
+        int $quantidade
+    ) {
         $this->id = $id;
         $this->nome = $nome;
+        $this->preco = $preco;
+        $this->quantidade = $quantidade;
     }
 
     public function getId(): int
@@ -29,5 +37,30 @@ class Produto
     public function setNome(string $nome): void
     {
         $this->nome = $nome;
+    }
+
+    public function getPreco(): float
+    {
+        return $this->preco;
+    }
+
+    public function setPreco(float $preco): void
+    {
+        $this->preco = $preco;
+    }
+
+    public function getQuantidade(): int
+    {
+        return $this->quantidade;
+    }
+
+    public function setQuantidade(int $quantidade): void
+    {
+        $this->quantidade = $quantidade;
+    }
+
+    public function calcularValorEstoque(): float
+    {
+        return $this->preco * $this->quantidade;
     }
 }
